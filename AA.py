f0d4f4daa955d9d81515184773cb97ba057ef749
maplewilliam <maplewilliam@naver.com>
2022-06-14 23:41:57 +0900

    0614

--- AA.py
+++ AA.py
@@ -1,19 +1,3 @@
 import sys
-sys.stdin=open("input.txt", 'rt')
-
-sticks = input()
-lst = []
-sum = 0
-
-for i in range(len(sticks)):
-    if sticks[i] == '(':
-        lst.append(sticks[i])
-    else:
-        if sticks[i-1] == '(':
-            lst.pop()
-            sum += len(lst)
-        else:
-            lst.pop()
-            sum += 1
-            
-print(sum)
+from collections import deque
+#sys.stdin=open("input.txt", 'rt')
